f2362b1126712f8a8e32d9b8da28d94d9f5bc682
jacobjohn2016 <kapsjacob@gmail.com>
2020-06-05 01:49:13 +0530

    added tasks

---
+++ Python/quiz_8.py
@@ -0,0 +1,24 @@
+# DO *NOT* WRITE YOUR NAME TO MAINTAIN ANONYMITY FOR PLAGIARISM DETECTION
+
+
+# Assignments to a CircularList object will only involve integer indexes.
+#
+# When the index is an integer, it is interpreted modulo the length
+# of the list.
+#
+# When working with slices:
+# - The last argument of a slice cannot be equal to 0.
+# - When the last argument of a slice is not given, it is set to 1.
+# - When the first argument of a slice is not given, it is set to
+#   0 or to -1 depending on the sign of the last argument.
+# - When the second argument of a slice is not given, it is set to
+#   len(list) or to -len(list) -1 depending on the sign of the last argument.
+# - Denoting by L the CircularList object, returns a list consisting of
+#   all elements of the form L[i modulo len(L)] for i ranging between first
+#   (included) and second (excluded) arguments of slice, in steps given by
+#   third argument of slice.
+
+class CircularList:
+    pass
+    # REPLACE PASS ABOVE WITH YOUR CODE
+
