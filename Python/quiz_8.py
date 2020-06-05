d5dcb470b0f3bcb6914c9eff7583366cfd191541
jacobjohn2016 <kapsjacob@gmail.com>
2020-06-05 10:41:52 +0530

    completed python exercise

--- Python/quiz_8.py
+++ Python/quiz_8.py
@@ -18,7 +18,38 @@
 #   (included) and second (excluded) arguments of slice, in steps given by
 #   third argument of slice.
 
-class CircularList:
-    pass
-    # REPLACE PASS ABOVE WITH YOUR CODE
+class CircularList(list):
+    def __init__(self, *data):
+        """Initialize the class"""
+        super(CircularList, self).__init__()
+        for d in data:
+            self.append(d)
 
+    def __getitem__(self, index):
+        """Get sliced items from class"""
+        if isinstance(index, slice):
+            return [self[i] for i in self._rangeify(index)]
+        elif isinstance(index, int):
+            index = int(index) % self.__len__()
+            return super(CircularList, self).__getitem__(index)
+        else:
+            raise TypeError("Invalid argument type.")
+        
+    def _rangeify(self, slice):
+        """Convert slice to a range"""
+        start, stop, step = slice.start, slice.stop, slice.step
+        if step is None:
+            step = 1
+        if start is None and step >= 0:
+            start = 0
+        elif start is None and step < 0:
+            start = -1
+        if stop is None and step >= 0:
+            stop = self.__len__()
+        elif stop is None and step < 0:
+            stop = -self.__len__() - 1
+        return range(start, stop, step)
+
+    def __setitem__(self, index, item):
+        index = int(index) % self.__len__()
+        super(CircularList, self).__setitem__(index, item)
